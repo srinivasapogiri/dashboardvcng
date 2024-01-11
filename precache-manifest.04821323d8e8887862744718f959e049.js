00e8014782998b9e3f842d4781c07f4825849f8c
Pogiri Jaya <srinivasjaya874@gmail.com>
2024-01-11 09:13:29 +0530

    Initial commit

---
+++ precache-manifest.04821323d8e8887862744718f959e049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21ecfeefdbfb38229f6a704601cb323",
+    "url": "/srinivasapogiri/dashboardvcng/index.html"
+  },
+  {
+    "revision": "f338212ff3c5bf1a198f",
+    "url": "/srinivasapogiri/dashboardvcng/static/css/main.403e0ee3.chunk.css"
+  },
+  {
+    "revision": "6b2c90daf5e6f6c24523",
+    "url": "/srinivasapogiri/dashboardvcng/static/js/2.059b8ae7.chunk.js"
+  },
+  {
+    "revision": "ea862b4d7291d9d99556",
+    "url": "/srinivasapogiri/dashboardvcng/static/js/3.bd0f349a.chunk.js"
+  },
+  {
+    "revision": "f338212ff3c5bf1a198f",
+    "url": "/srinivasapogiri/dashboardvcng/static/js/main.9bba6938.chunk.js"
+  },
+  {
+    "revision": "fe99a5c99e5bf53ad513",
+    "url": "/srinivasapogiri/dashboardvcng/static/js/runtime~main.6816aee5.js"
+  }
+]);
